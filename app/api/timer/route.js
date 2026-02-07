76e0eea46e8f1ce486b009c58ae93d6911e4d430
raxitzora <raxitzora@gmail.com>
2026-02-07 17:04:01 +0530

    first commit and added api/timer/route.js

--- app/api/timer/route.js
+++ app/api/timer/route.js
@@ -1,1 +1,13 @@
 import NextResponse from "next/server";
+
+export async function GET(){
+    const currentTime = new Date();
+    
+    return NextResponse.json({
+        timestamp:currentTime.toISOString(),
+        readable:currentTime.toLocaleDateString(),
+        unix:currentTime.getTime(),
+        message:"Timer API called successfully!",
+        requestId:Math.random().toString(36).substring(2,15)
+    });
+}
