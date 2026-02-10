6732bc6fb458c8e8f0f58876a0fdced5004a6a80
raxitzora <raxitzora@gmail.com>
2026-02-10 20:17:57 +0530

    started

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Navigation from '@/components/navigation'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <Navigation />
+      
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ components/navigation.jsx
@@ -0,0 +1,15 @@
+"use client"
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const Navigation = () => {
+    const pathname = usePathname()
+  return (
+    <div>
+        navigation
+      
+    </div>
+  )
+}
+
+export default Navigation
